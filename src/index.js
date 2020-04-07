9dcecb1c1675601b43927331046d52310186b6c7
yokoyan <2511649+taka-yoko@users.noreply.github.com>
2020-04-08 01:31:20 +0900

    javascriptのclass

--- src/index.js
+++ src/index.js
@@ -3,7 +3,27 @@ import { render } from "react-dom";
 
 import ReactComponent from "./ReactComponent";
 
+class Human {
+  constructor(name, music) {
+    this.name = name;
+    this.music = music;
+  }
+
+  callMyProfile() {
+    console.log(this.name, this.music);
+  }
+}
+
+const tamori = new Human("タモリ", "ジャズ");
+const takeshi = new Human("たけし", "サックス");
+
+tamori.callMyProfile();
+takeshi.callMyProfile();
+
 render(
-  <ReactComponent name="nakanishi" music="jazz" />,
+  <div>
+    <ReactComponent name={tamori.name} music={tamori.music} />
+    <ReactComponent name={takeshi.name} music={takeshi.music} />
+  </div>,
   document.getElementById("root")
 );
